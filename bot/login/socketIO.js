48b7403e7ab3e35f8874efed2b8144aac4cd1a0b
eren999x <Ffan4684@gmail.com>
2025-08-18 00:37:17 +0600

    Update socketIO.js

--- bot/login/socketIO.js
+++ bot/login/socketIO.js
@@ -1,58 +1,44 @@
+// This module will be called if enabled in the config (severUptime.socket.enable = true)
 /**
- * Socket.IO server module for GoatBot uptime monitoring
- * This module will be called if enabled in config (serverUptime.socket.enable = true)
- * @example See ./connectSocketIO.example.js
+ * @example for connect to socket.io
+ * view file ./connectSocketIO.example.js
  */
 const { Server } = require("socket.io");
 const { log, getText } = global.utils;
 const { config } = global.GoatBot;
 
 module.exports = async (server) => {
-    const { channelName, verifyToken } = config.serverUptime.socket || {};
-    let io;
+        const { channelName, verifyToken } = config.serverUptime.socket;
+        let io;
 
-    // Validate config
-    if (!channelName) {
-        return log.err("SOCKET IO", getText("socketIO", "error"), '"channelName" is not defined in config');
-    }
-    if (!verifyToken) {
-        return log.err("SOCKET IO", getText("socketIO", "error"), '"verifyToken" is not defined in config');
-    }
-
-    try {
-        io = new Server(server);
-        log.info("SOCKET IO", getText("socketIO", "connected"));
-    } catch (err) {
-        return log.err("SOCKET IO", getText("socketIO", "error"), err);
-    }
-
-    io.on("connection", (socket) => {
-        const token = socket.handshake.query?.verifyToken;
-
-        // Validate token
-        if (token !== verifyToken) {
-            socket.emit(channelName, {
-                status: "error",
-                message: "Token is invalid"
-            });
-            socket.disconnect(true);
-            log.warn("SOCKET IO", `Client ${socket.id} provided invalid token`);
-            return;
+        try {
+                if (!channelName)
+                        throw ('"channelName" is not defined in config');
+                if (!verifyToken)
+                        throw ('"verifyToken" is not defined in config');
+                io = new Server(server);
+                log.info("SOCKET IO", getText("socketIO", "connected"));
+        }
+        catch (err) {
+                return log.err("SOCKET IO", getText("socketIO", "error"), err);
         }
 
-        log.info("SOCKET IO", `New client connected: ${socket.id}`);
-        socket.emit(channelName, {
-            status: "success",
-            message: "Connected to server successfully"
+        io.on("connection", (socket) => {
+                if (socket.handshake.query.verifyToken != verifyToken) {
+                        io.to(socket.id).emit(channelName, {
+                                status: "error",
+                                message: "Token is invalid"
+                        });
+                        socket.disconnect();
+                        return;
+                }
+                log.info("SOCKET IO", `New client connected to socket: ${socket.id}`);
+                io.to(socket.id).emit(channelName, {
+                        status: "success",
+                        message: "Connected to server successfully"
+                });
+                socket.on("disconnect", () => {
+                        log.info("SOCKET IO", `Client disconnected from socket: ${socket.id}`);
+                });
         });
-
-        socket.on("disconnect", (reason) => {
-            log.info("SOCKET IO", `Client disconnected: ${socket.id} | Reason: ${reason}`);
-        });
-    });
-
-    // Optional: handle server-side errors
-    io.on("error", (err) => {
-        log.err("SOCKET IO", "Socket.IO server error", err);
-    });
 };
